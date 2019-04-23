50fe149a40fb0089fe978c240b000eded7f7a2d4
Ryan Bravo <rrbravo2@gmail.com>
2019-04-23 05:16:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
   if (string.toUpperCase() === string) {
-    return "Yes indeed!"
+    return "YES INDEED!"
   }
   if (string === "I love you, Grandma.") {
     return "I love you, too."
